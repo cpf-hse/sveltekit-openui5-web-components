6d7cc3bfc5601daaa4dc2ad5234a91e50fa74566
Christian Pfisterer <christian.pfisterer@hse.com>
2023-02-27 12:16:52 +0100

    added icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
-<script>
-	import "@ui5/webcomponents-fiori/dist/Page.js";
-	import "@ui5/webcomponents-fiori/dist/Bar.js";
-	import "@ui5/webcomponents/dist/Label";
-	import "@ui5/webcomponents/dist/Button";
+<script lang="ts">
+	import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme';
+	import '@ui5/webcomponents-icons/dist/employee';
+	import '@ui5/webcomponents/dist/Assets';
+	import '@ui5/webcomponents-fiori/dist/Page';
+	import '@ui5/webcomponents-fiori/dist/Bar';
+	import '@ui5/webcomponents/dist/Label';
+	import '@ui5/webcomponents/dist/Button';
+
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log('the component has mounted');
+		setTheme('sap_horizon_dark');
+	});
 </script>
 
 <svelte:head>
@@ -10,11 +20,15 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<ui5-page>
-	<ui5-bar design="Header" slot="header">
-		<ui5-label>Header Title</ui5-label>	
-	</ui5-bar>
-	<div>
-		<ui5-button icon="employee">Add</ui5-button>
-	</div>
-</ui5-page>
+<div>
+	<ui5-page>
+		<ui5-bar design="Header" slot="header">
+			<ui5-label>Header Title</ui5-label>
+		</ui5-bar>
+		<div style="background-color: blue;">
+			<section>
+				<p>Test Test test</p>
+			</section>
+		</div>
+	</ui5-page>
+</div>
