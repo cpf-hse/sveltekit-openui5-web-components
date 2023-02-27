0aaea624e63779951966073ef51ab758892f04a6
cpf-hse <103113173+cpf-hse@users.noreply.github.com>
2023-02-27 11:26:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltekit-openui5-web-components
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-my9kte)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import "@ui5/webcomponents-fiori/dist/Page.js";
+	import "@ui5/webcomponents-fiori/dist/Bar.js";
+	import "@ui5/webcomponents/dist/Label";
+	import "@ui5/webcomponents/dist/Button";
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<ui5-page>
+	<ui5-bar design="Header" slot="header">
+		<ui5-label>Header Title</ui5-label>	
+	</ui5-bar>
+	<div>
+		<ui5-button icon="employee">Add</ui5-button>
+	</div>
+</ui5-page>
